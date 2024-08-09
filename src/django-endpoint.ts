9ebd0f56adfcbb4b91b66aed5d311fb5cb017ede
Ahvan Misra <misra28@purdue.edu>
2024-08-09 12:37:38 -0700

    added new endpoint

--- src/django-endpoint.ts
+++ src/django-endpoint.ts
@@ -1,6 +1,7 @@
 const getDjangoEndpoint = () => {
-    return 'http://34.218.244.156:8000/';
+    // return 'http://34.218.244.156:8000/';
     // return 'http://localhost:8000/';
+    return 'https://mymovielist-django-json-1h5tx5omy-ahvan-misras-projects.vercel.app/';
 }
 
 export default getDjangoEndpoint;
