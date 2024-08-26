fee853deae5c7d6f0da5c24de723b10d3ffb6e8b
Ahvan Misra <misra28@purdue.edu>
2024-08-26 16:51:39 -0400

    changed endpoint 3

--- src/django-endpoint.ts
+++ src/django-endpoint.ts
@@ -1,7 +1,7 @@
 const getDjangoEndpoint = () => {
     // return 'http://34.218.244.156:8000/';
     // return 'http://localhost:8000/';
-    return 'https://mymovielist-django-api.vercel.app:8000/';
+    return 'https://mymovielist-django-api.vercel.app/';
 }
 
 export default getDjangoEndpoint;
